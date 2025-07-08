453c592e58235e957511861b66a0fa638233e0f3
“Fahil <failfarkshatov@gmail.com>
2025-07-08 04:48:57 -0600

    refactor two-step example

--- README.md
+++ README.md
@@ -25,37 +25,42 @@ Usage examples can be found in the following files
 ```r
 # input parameters:
 
-# Lagrangian multipliers
-l0 <- 1526 # lambda0
-l1 <- 326  # lambda1
-
 # hypothesized values:
-th0 <- 0    # theta0
-th1 <- 0.21 # theta1
+  th0 <- 0             # theta0
+  th1 <- 0.21          # theta1
+  thgam <- c(th0, th1) # points at which ASC is weighted
+  gam <- c(0.5, 0.5)   # weights for weighting ASC
 
-precision = 0.05   #  grid step
-thgam = c(th0,th1) #  points at which ASC is weighted
-gam = c(0.5, 0.5)  #  weights for weighting ASC
-M = c(100, 100)    #  sample size for the first and the second step
+# Lagrangian multipliers
+  l0 <- 1526  # lambda0
+  l1 <- 326   # lambda1
 
-cost_fn <- function (x) x # cost of a group group of x observations
+  M <- c(100, 100)           # group sizes
+  precision <- 0.05          # grid step
+  cost_fn <- function (x) x  # cost of a group group of x observations
 
-# main function call for optimal plan cumputation:
-test = calculate_plan(M, l0, l1, th0, th1, gam, thgam,
-                      cost_fn=cost_fn,
-                      precision=precision, 
-                      print_output=FALSE)
+  # main function call for optimal plan cumputation:
+  test <- calculate_plan(M, l0, l1, th0, th1, gam, thgam,
+                         cost_fn=cost_fn,
+                         precision=precision,
+                         print_output=FALSE)
 
-# report on the computed plan
-results = plan_summary(test, M, l0, l1, th0, th1, costs, cost_fn)
+  alpha <- 1 - operating_characteristic(test, th0)
+  beta <- operating_characteristic(test, th1)
+  print(paste("alpha:", alpha, ", beta:", beta))
+  # [1] "alpha: 0.0250017679625842 , beta: 0.200056694872385"
 
-# print the computed plan characteristics:
-for (name in names(results)){
-print(paste(name, results[[name]]))
-}
+  ASN0 <- average_sampling_cost(test, th0, cost_fn=cost_fn)
+  ASN1 <- average_sampling_cost(test, th1, cost_fn=cost_fn)
+  print(paste("ASN0:", ASN0, ", ASN1:", ASN1))
+  # [1] "ASN0: 118.489308233436 , ASN1: 148.471027781829"
 
-print("Continuation interval for the first step")
-print(paste(head(test$data[[1]]$grid, 1), tail(test$data[[1]]$grid, 1)))
-print("Critical constant for the second step")
-print(test$data[[2]]$const)
+  print(paste("Continuation interval for the first step:",
+              head(test$data[[1]]$grid, 1),
+              tail(test$data[[1]]$grid, 1)))
+  # [1] "Continuation interval for the first step: 8.60693365315347 23.3352164728895"
+
+  print(paste("Critical constant for the second step:",
+              test$data[[2]]$const))
+  # [1] "Critical constant for the second step: 28.350037287989"
 ```
--- normal/example.R
+++ normal/example.R
@@ -25,35 +25,46 @@ plan_summary <- function(test, M, l0, l1, th0, th1, costs, cost_fn) {
 
 print("An example of optimal plan computation")
 # input parameters:
-# Lagrangian multipliers
-  l0 <- 1526 # lambda0
-  l1 <- 326  # lambda1
+
 # hypothesized values:
-  th0 <- 0    # theta0
-  th1 <- 0.21 # theta1
-  precision = 0.05   #  grid step
-  thgam = c(th0,th1) #  points at which ASC is weighted
-  gam = c(0.5, 0.5)  #  weights for weighting ASC
-  M = c(100, 100)    #  sample size for the first and the second step
-  costs = c(100, 100) # group costs
-  cost_fn <- function (x) x # cost of a group group of x observations
+  th0 <- 0             # theta0
+  th1 <- 0.21          # theta1
+  thgam <- c(th0, th1) # points at which ASC is weighted
+  gam <- c(0.5, 0.5)   # weights for weighting ASC
+
+# Lagrangian multipliers
+  l0 <- 1526  # lambda0
+  l1 <- 326   # lambda1
+
+  M <- c(100, 100)           # group sizes
+  precision <- 0.05          # grid step
+  cost_fn <- function (x) x  # cost of a group group of x observations
+
   # main function call for optimal plan cumputation:
-  test = calculate_plan(M, l0, l1, th0, th1, gam, thgam,
-                        costs=NULL,
-                        cost_fn=cost_fn,
-                        precision=precision, print_output=FALSE)
-# report on the computed plan
-  results = plan_summary(test, M, l0, l1, th0, th1, costs, cost_fn)
+  test <- calculate_plan(M, l0, l1, th0, th1, gam, thgam,
+                         cost_fn=cost_fn,
+                         precision=precision,
+                         print_output=FALSE)
 
-# print the computed plan characteristics:
-  for (name in names(results)){
-    print(paste(name, results[[name]]))
-  }
+  alpha <- 1 - operating_characteristic(test, th0)
+  beta <- operating_characteristic(test, th1)
+  print(paste("alpha:", alpha, ", beta:", beta))
+  # [1] "alpha: 0.0250017679625842 , beta: 0.200056694872385"
 
-print("Continuation interval for the first step")
-    print(paste(head(test$data[[1]]$grid, 1), tail(test$data[[1]]$grid, 1)))
-print("Critical constant for the second step")
-  print(test$data[[2]]$const)
+  ASN0 <- average_sampling_cost(test, th0, cost_fn=cost_fn)
+  ASN1 <- average_sampling_cost(test, th1, cost_fn=cost_fn)
+  print(paste("ASN0:", ASN0, ", ASN1:", ASN1))
+  # [1] "ASN0: 118.489308233436 , ASN1: 148.471027781829"
+
+  print(paste("Continuation interval for the first step:",
+              head(test$data[[1]]$grid, 1),
+              tail(test$data[[1]]$grid, 1)))
+  # [1] "Continuation interval for the first step: 8.60693365315347 23.3352164728895"
+
+  print(paste("Critical constant for the second step:",
+              test$data[[2]]$const))
+  # [1] "Critical constant for the second step: 28.350037287989"
+
 
 
 
